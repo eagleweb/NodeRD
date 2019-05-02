1517ef800df0fbac32f6e52a88a809738f7175f7
eagleweb <orel.viacheslav@gmail.com>
2019-05-02 12:06:00 +0300

    add changes

--- src/models/list.js
+++ src/models/list.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const ListSchema = new Schema({
   title: { type: String, required: true, unique: true },
   order: { type: String },
-  task: { type: Array, default: [] }
+  task: { type: Array, default: [] },
+  board: { type: Array, default: [] }
 });
 
 module.exports = mongoose.model('List', ListSchema);
--- src/models/task.js
+++ src/models/task.js
@@ -5,7 +5,8 @@ const TaskSchema = new Schema({
   title: { type: String, required: true, unique: true },
   order: { type: String },
   description: { type: String },
-  assignee: { type: String, required: true }
+  assignee: { type: String, required: true },
+  list: { type: Array, default: [] }
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
